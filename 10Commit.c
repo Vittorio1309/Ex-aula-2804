d7e23017f06b417b1200c4289cc7faef4c1fc0ec
Vittorio Allievi Uecker <vittorio.uecker@alunos.bpkedu.com.br>
2025-05-12 20:24:38 -0300

    2 commit

--- 10Commit.c
+++ 10Commit.c
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 
 <head>
-    <title> Sim </title>
 </head>
+
+<body> 
+    <h1> Bordados Vittorio </h1>
+    <a href =https://web.whatsapp.com/ ><img>https://img.freepik.com/vetores-premium/icone-do-logotipo-do-whatsapp-circulo-aplicativo-de-midia-social-aplicativo-de-rede-marca-editorial-popular-ilustracao-vetorial_913857-391.jpg?semt=ais_hybrid&w=740 </img> </a> 
+</body>
